e5660e38d9b1881ff4dc46c52f66d77f05149d57
Julia <ulia.kononenko2000@gmail.com>
2023-02-07 20:03:09 +0200

    created project and added to repository

---
+++ src/main/java/com/example/javachallengetask/JavaChallengeTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.javachallengetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaChallengeTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaChallengeTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javachallengetask/JavaChallengeTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javachallengetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaChallengeTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
